eb2d44942decd53125aa38575ba75cc3ada28bf5
muzakh <engr.zak_aug1989@hotmail.com>
2024-03-19 23:22:39 +0100

    updated code

--- README.md
+++ README.md
@@ -15,4 +15,27 @@ var i int = 100
 var b bool = false
 var f float64 = 77.90
 
+- OR 
+  var name string
+  name = "zohaib"
+
+- OR 
+  var greeting, name, subject string = "Hello", "Lisa", "Mathematics"
+
+- OR
+  var (
+      name string = "zohaib"
+      i int = 100
+      b bool = false
+      f float64 = 77.99
+  )
+
+# format specifiers -- Use fmt.Printf
+- %v - formats value in a default format
+- %d - formats decimal integers
+- %s - plain string
+- %q - quoted characters/string
+- %f - floating numbers
+- $2f - floating numbers up to 2 decimal places
+etc..
 
--- main.go
+++ main.go
@@ -10,9 +10,31 @@ import "fmt"
 func main()  {
 	name:= "Lisa"  // This is dynamically typing
 	var greeting string = "Hello" // This is statically typing
+	var subject string = "Mathematics"
+	// OR var greeting, name, subject string = "Hello", "Lisa", "Mathematics"
+
+	var marks int = 82 
+	/*
+OR
+var marks int
+marks = 82
+	*/
+
+
+/*
+var (
+	greeting string = "Lisa"
+    subject string = "Mathematics"
+	marks int = 82
+)
+
+*/
 
 	fmt.Println(greeting) 	// OR fmt.Print(greeting, "\n")
 	fmt.Println(name)
+	fmt.Printf("Congrats! You have scored %d marks in %v", marks, subject) // fmt.Printf is to enable format specifie else it will mot work
+
+
 
 	
 }
