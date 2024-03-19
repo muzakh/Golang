d4635cc009f285e7da7c262fd27052ff911d316e
muzakh <engr.zak_aug1989@hotmail.com>
2024-03-19 22:33:14 +0100

    first golang code

---
+++ main.go
@@ -0,0 +1,13 @@
+// My first Go code
+
+// Here main package represent that the code is executable instead of a shared library
+
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Hello World!")
+	
+}
+
